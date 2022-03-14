ebbf55085e4602f6149c55c551dbd981b5602454
Matheus-F-M <mfm3@cesar.school>
2022-03-14 18:06:19 -0300

    Update 1

--- thread_exercise.c
+++ thread_exercise.c
@@ -6,7 +6,7 @@
 float x;
 int y, z;
 int n;
-int numbers[50];
+int numbers[0];
 
 /*Functions index*/
 void *a_average();
@@ -18,7 +18,6 @@ int main()
     pthread_t t_01;
     pthread_t t_02;
     pthread_t t_03;
-    void *result;
     
     printf("Digite a quantidade de valores:");
     scanf("%d", &n);
@@ -29,18 +28,17 @@ int main()
 	}
 
     pthread_create(&t_01, NULL, &a_average, NULL);
-    pthread_join(t_01, &result);
+    pthread_join(t_01, NULL);
 
     pthread_create(&t_02, NULL, &minimum, NULL);
-    pthread_join(t_02, &result);
+    pthread_join(t_02, NULL);
 
     pthread_create(&t_03, NULL, &maximum, NULL);
-    pthread_join(t_03, &result);
+    pthread_join(t_03, NULL);
 
     printf("Média aritimétia: %.2f\n", x);
     printf("Menor número: %d\n", y);
     printf("Maior número: %d\n", z);
-
     return 0;
 }
 
